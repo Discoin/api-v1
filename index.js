b1d448173d8a6fce09cbdb915b7e5bdf6ae92aa8
CyberiumShadow <CyberiumShadow@users.noreply.github.com>
2017-07-21 14:09:45 +1000

    Var/Let/Consts

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 // First prototype of Discoin.
 // Perfectly lol
-var restify = require('restify');
+const restify = require('restify');
 
 // Define exchange rate.
 // From: 1 Bot currency = ? Discoin
 // To: 1 Discoin = ? Bot currency
-var rates = [
-];
+const rates = [];
 // Define unprocessed transactions.
-var transactions = [];
+const transactions = [];
 
-var server = restify.createServer();
+const server = restify.createServer();
 server.get('/', function status(req, res, next) {
 	res.sendRaw('I should work correctly. If I missed a transaction, please contact my master (austinhuang#1076).');
 	next();
 });
 
 server.get('/transaction/:user/:amount/:to', function respond(req, res, next) {
-	var from = rates.find(f => {return f.token === req.headers.authorization});
+	const from = rates.find(f => {return f.token === req.headers.authorization});
 	if (from === undefined) {
 		res.sendRaw('[ERROR] Unauthorized token!');
 		return;
@@ -26,29 +25,29 @@ server.get('/transaction/:user/:amount/:to', function respond(req, res, next) {
 		res.sendRaw('[ERROR] "Amount" not a number!');
 		return;
 	}
-	var rate = rates.find(r => {return r.code === req.params.to});
+	const rate = rates.find(r => {return r.code === req.params.to});
 	if (rate === undefined) {
 		res.sendRaw('[ERROR] "To" currency NOT FOUND.');
 		return;
 	}
-	var amount = parseInt(req.params.amount) * from.from * rate.to;
+	const amount = parseInt(req.params.amount) * from.from * rate.to;
 	transactions.push({user: req.params.user, for: req.params.to, amount: amount});
 	res.sendRaw("Submitted.");
 });
 
 server.get('/transaction', function respond(req, res, next) {
-	var bot = rates.find(f => {return f.token === req.headers.authorization});
+	const bot = rates.find(f => {return f.token === req.headers.authorization});
 	if (bot === undefined) {
 		res.sendRaw('[ERROR] Unauthorized token!');
 		return;
 	}
-	var mytransactions = transactions.filter(t => {return t.for === bot.code});
+	let mytransactions = transactions.filter(t => {return t.for === bot.code});
 	res.sendRaw(JSON.stringify(mytransactions));
 	mytransactions.forEach(m => {transactions.splice(transactions.indexOf(m), 1);})
 });
 
 server.get('/rates', function respond(req, res, next) {
-	var info = "Current exchange rates for Discoin follows:\n";
+	let info = "Current exchange rates for Discoin follows:\n";
 	rates.forEach(i => {info += "\n"+i.name+": 1 "+i.code+" => "+i.from+" Discoin => "+i.from*i.to+" "+i.code});
 	res.sendRaw(info);
 });
