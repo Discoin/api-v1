5b01710e1d73be94e2b8f6f27fc6c9f80d8ce32e
Austin Huang <austinhuang0131@gmail.com>
2017-07-21 15:52:45 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/* Cookie Consent Start
+-------------------------------------------------------------- */
+window.cookieconsent.initialise({
+	"palette": {
+		"popup": {
+			"background": "#23272A"
+		},
+		"button": {
+			"background": "transparent",
+			"text": "#14a7d0",
+			"border": "#14a7d0"
+		}
+	},
+	"content": {
+		"message": "DiscordForge uses cookies to ensure you get the best experience on the website."
+	}
+});
+/* Cookie Consent End
+-------------------------------------------------------------- */
